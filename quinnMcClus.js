9d122046a186ef15a060bbf4135eeba552c16dc7
Siddarth Narasimhan <sidarceus@gmail.com>
2019-12-16 22:53:39 -0500

    Working Quinn-McCluskey Algorithm!

--- quinnMcClus.js
+++ quinnMcClus.js
@@ -1,8 +1,8 @@
 /* This algorithm simplifies boolean logic through the Quinn-McCluskey Method */
 
 // let A = [0,4,5,7,8,11,12,15];
-// let A = [0,1,3,4,6,7,8,9,11,12,13,14,15]
-let A = [0, 1, 2, 3, 6, 8, 9, 10, 11, 17, 20, 21, 23, 25, 28, 30, 31];  // Input list
+let A = [0, 1, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 15]
+//let A = [0, 1, 2, 3, 6, 8, 9, 10, 11, 17, 20, 21, 23, 25, 28, 30, 31];  // Input list
 // let A = [2,4,6,9,10,11,12,13,15]; - Don't Care Example
 
 let B = [];                 // Iteration list
@@ -10,6 +10,8 @@ let notDoneList = [];       // Another iteration list, supporting B
 let doneList = [];          // The elements which go through to the final stage
 let doneIter = false;       // Done iterations?
 let numV = 0;               // Number of variables
+let boolExpr = "";
+let check = false;
 
 // Class used to produce ternary elements (Consisting of '1', '0' or '-')
 class Num {
@@ -41,7 +43,8 @@ while (Math.max(...A) >= Math.pow(2, numV)) {
 
 // Main boolean simplification function
 function main() {
-    // Store copy of A
+    
+    // Use copy of A to preserve original list
     let Acopy = [...A];
 
     // Converts every array element from decimal to class element
@@ -73,6 +76,19 @@ function main() {
 
     // Organizes data into a table
     let elemArr = graph();
+
+    // Finds the potential for each row in the table : the potential is determined by a prioritization algorithm - priority is given to rows with essential prime implicants and then those with the maximum number of 'x's the row can cancel
+    let pntl = potentialFcn(elemArr);
+
+    // Iteration loop that finds optimal boolean expression.
+    while (!check) {
+        boolExpr += doneList[pntl[0][2]].logic + " + "; // Update boolean expression
+        elemArr = newGraph(elemArr, pntl[0][2]);        // Reproduce graph
+        pntl = potentialFcn(elemArr)                    // Reproduce new potential function for the new graph
+        checkElem(elemArr);                             // Checks whether the all 'x's are cancelled.
+    }
+
+    boolExpr = boolExpr.substring(0, boolExpr.length - 3);  // Remove the last three characters, ie. ' + ', to produce final expression
 
     return;
 
@@ -177,43 +193,109 @@ function remExtra() {
     return;
 }
 
-function calcLogic () {
-    doneList.forEach(function(value) {
+// Finds the boolean expression for the specific element in the doneList
+function calcLogic() {
+    doneList.forEach(function (value) {
         for (let i = 0; i < numV; i++) {
             if (value.bin.charAt(i) === '0')
-                value.logic = value.logic + alphabet(i) + "'";
+                value.logic = value.logic + literal(i) + "'";
             else if (value.bin.charAt(i) === '1')
-                value.logic = value.logic + alphabet(i);
+                value.logic = value.logic + literal(i);
         }
     });
 
     return;
 }
 
-function alphabet(i) {
-    let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+// Outputs the literals in alphabetical order
+function literal(i) {
+    let alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     return alpha[i];
 }
 
+// Initializes the graph
 function graph() {
     let graphArr = [];
-    doneList.forEach(function(value) {
-        let x = [...value.decComb];
-        for (let i = 0; i < A.length; i++) {
-            if (x[i] != A[i] && !isNaN(x[i]))
-                x.splice(i,0,'');
-            if (i >= x.length)
-                x.push('');
-        }
-        graphArr.push(x);
+    doneList.forEach(function (value) {
+        graphArr.push([...value.decComb]);
     });
 
     return graphArr;
 }
 
-main();
-console.log(A);
-console.log(doneList)
+function potentialFcn(element) {
+    let elemCount = [];
+    let pntl = [];
+    let count = 0;
 
-//console.log(B)
-//console.log(notDoneList)
+    A.forEach(function (value) {
+        for (let i = 0; i < element.length; i++) {
+            if (element[i].includes("" + value + "")) {
+                count++;
+            }
+        }
+        elemCount.push(count);
+        count = 0;
+    });
+
+    let pntlCount = 0;
+    let pntlPI = false;
+    for (let i = 0; i < element.length; i++) {
+        pntl.push([]);
+        for (let j = 0; j < element[i].length; j++) {
+            let I = A.findIndex(function (value) { if (value == element[i][j]) { return true; } });
+            pntlCount += elemCount[I];
+            if (elemCount[I] === 1)
+                pntlPI = true;
+        }
+        pntl[i].push(pntlCount);
+        pntl[i].push(pntlPI);
+        pntl[i].push(i);
+        pntlCount = 0;
+        pntlPI = false;
+    }
+
+    pntl.sort(function (a, b) {
+        if (a[1] && !b[1])
+            return -1;
+        else if (!a[1] && b[1])
+            return 1;
+        else
+            return b[0] - a[0];
+    });
+
+    return pntl;
+
+}
+
+function newGraph(elemArr, indx) {
+    let E = [...elemArr];
+    let Echk = [...E[indx]];
+
+    for (let i = 0; i < elemArr.length; i++) {
+        for (let j = 0; j < elemArr[i].length; j++) {
+            if (Echk.includes(E[i][j])) {
+                E[i][j] = '';
+            }
+        }
+    }
+
+    return E;
+}
+
+function checkElem(elemArr) {
+    for (let i = 0; i < elemArr.length; i++) {
+        for (let j = 0; j < elemArr[i].length; j++) {
+            if (elemArr[i][j] !== '') {
+                check = false;
+                return;
+            }
+        }
+    }
+    check = true;
+    return;
+}
+
+main();
+
+console.log(boolExpr);
